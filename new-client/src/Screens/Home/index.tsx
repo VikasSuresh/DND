e4f06a34650f33602ed1ec27be244a8c35ca54f1
Vikas Suresh <vsvikassvs@gmail.com>
2021-08-30 23:12:57 +0530

    added ts static type and hooks

--- new-client/src/Screens/Home/index.tsx
+++ new-client/src/Screens/Home/index.tsx
@@ -1,17 +1,47 @@
 import React, { useState,useEffect } from "react";
 
+interface Task {
+    task: string,
+    completed: boolean
+}
+
 export const Home = () =>{
-    const [todo,setToDo] = useState([]);
+    const [todos, setToDo] = useState<Task[]>([{
+        task:"A",
+        completed:false
+    }]);
 
     useEffect(()=>{
-        
-    })
+        setTimeout(()=>{
+            setToDo((state)=>([
+                ...state,
+                {
+                    task:"B",
+                    completed:false
+                }
+            ]))
+        })
+    },[])
 
+    let render = ["loading"]
+
+    if(todos.length>0){
+        render = todos.map(el=>el.task)
+    }
+
+    const submit = (e:any) =>{
+        e.preventDefault();
+        setToDo((state)=>([...state,{
+            task:"C",
+            completed:false
+        }]))
+    }
+    
+    console.log(render)
     return(
         <div>
-            <h1>
-                
-            </h1>
+           <p>{render}</p>
+           <button type='submit' onClick={submit}>ADD</button>
         </div>
     )
 }
