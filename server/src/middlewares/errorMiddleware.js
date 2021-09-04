9d890428e40638f43bbc42998b7f504574945b96
Vikas <vsvikassvs@gmail.com>
2021-09-04 15:34:10 +0530

    fix

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -1,15 +1,16 @@
-const { isCelebrateError, errors } = require('celebrate');
+const { isCelebrateError } = require('celebrate');
 
 module.exports = (err, req, res, next) => {
     try {
-        if (isCelebrateError(err)) {
-            return errors();
-        }
         const resObj = {
             success: false,
             value: null,
             message: err.message,
         };
+
+        if (isCelebrateError(err)) {
+            resObj.message = 'Wrong Data';
+        }
 
         return res.status(500).send(resObj);
     } catch (error) {
