d5086281e6f964223c90feb7ad939364f36bea45
Vikas <vsvikassvs@gmail.com>
2021-09-05 17:41:34 +0530

    fix

--- new-client/src/App.tsx
+++ new-client/src/App.tsx
@@ -8,7 +8,7 @@ const App = () => (
         <Right render={(properties: any) => (
             <Router>
                 <Switch>
-                    <Route path="/bookmarks" render={(props) => <Home path="bookmarks" properties={properties} {...props} />} />
+                    <Route path="/bookmarks" render={(props) => <Home path="bookmarks" handleDrawerOpen={properties} {...props} />} />
                     {/* <Route path="/tasks" render={(props) => <Home path="tasks" {...props} />} /> */}
                     <Route path="/" render={(props) => <Home handleDrawerOpen={properties} {...props} />} />
                 </Switch>
