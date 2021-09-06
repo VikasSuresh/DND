68d86ae5371d7d634591c8654a98685bc36640b5
Vikas <vsvikassvs@gmail.com>
2021-09-06 13:20:29 +0530

    added icon for completed

--- new-client/src/Components/index.ts
+++ new-client/src/Components/index.ts
@@ -2,6 +2,15 @@
 /* eslint-disable import/no-named-default */
 import { default as Add } from './Add';
 import { default as List } from './List';
+import * as Bookmark from './Bookmark';
+import * as Completed from './Completed';
+import * as Priority from './Priority';
 
 export const AddTask = Add;
 export const ListTask = List;
+
+export const Icons = {
+    ...Bookmark,
+    ...Completed,
+    ...Priority,
+};
