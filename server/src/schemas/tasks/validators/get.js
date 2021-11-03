acd547bda277a2d771cc61320e15e6d6274d8b39
Vikas <vsvikassvs@gmail.com>
2021-11-03 13:22:43 +0530

    default 100

--- server/src/schemas/tasks/validators/get.js
+++ server/src/schemas/tasks/validators/get.js
@@ -3,7 +3,7 @@ const { Segments, Joi, celebrate } = require('celebrate');
 module.exports = celebrate({
     [ Segments.QUERY ]: Joi.object({
         page: Joi.number().default(0),
-        size: Joi.number().default(10),
+        size: Joi.number().default(100),
         all: Joi.bool().default(false),
         search: Joi.string(),
         filter: Joi.string(),
