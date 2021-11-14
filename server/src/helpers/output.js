06656df1baa723567be5dda4b06039f699b2fb0a
Vikas <vsvikassvs@gmail.com>
2021-11-14 20:56:53 +0530

    added end

--- server/src/helpers/output.js
+++ server/src/helpers/output.js
@@ -11,8 +11,22 @@ module.exports = (val) => {
 function genDateString(val) {
     const { dueDate } = val;
     const seconds = moment(dueDate).diff(new Date(), 'seconds');
-    if (seconds <= 0) return { ...val, expired: true, dueDate: moment(dueDate).format('llll') };
-    if (seconds > 0 && seconds <= 86400) return { ...val, expired: false, dueDate: 'Today' };
-    if (seconds > 86400 && seconds <= 172800) return { ...val, expired: false, dueDate: 'Tomorrow' };
-    return { ...val, expired: false, dueDate: moment(dueDate).format('llll') };
+    if (seconds <= 0) {
+        return {
+            ...val, expired: true, dueDate: moment(dueDate).format('llll'), end: dueDate,
+        };
+    }
+    if (seconds > 0 && seconds <= 86400) {
+        return {
+            ...val, expired: false, dueDate: 'Today', end: dueDate,
+        };
+    }
+    if (seconds > 86400 && seconds <= 172800) {
+        return {
+            ...val, expired: false, dueDate: 'Tomorrow', end: dueDate,
+        };
+    }
+    return {
+        ...val, expired: false, dueDate: moment(dueDate).format('llll'), end: dueDate,
+    };
 }
